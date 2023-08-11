71ac1800a0b98537ea66c31baa366dbcdf487cb7
Hamza <hamzaaelshaaer@gmail.com>
2023-08-10 23:48:09 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ *Description: A C program that prints with put function
+ *
+ *Return: Alawys 0 (Success)
+*/
 int main(voids)
 {
 	puts("\"Programming is like building a multilingual puzzle/n");
